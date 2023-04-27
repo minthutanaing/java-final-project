88a804dfe00dabf94cce008e6f743886f7a5242d
Min Thuta Naing <6531503157@lamduan.mfu.ac.th>
2023-04-27 13:26:08 +0700

    init

---
+++ src/Hotel.java
@@ -0,0 +1,10 @@
+public class Hotel {
+    private String name; 
+    private String address;
+    private int contactNum; 
+    private String email; 
+    private double checkInTime;
+    private double checkOutTime; 
+    private String roomType; 
+    private boolean roomAvailability; 
+}
