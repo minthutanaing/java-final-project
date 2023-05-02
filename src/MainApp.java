f84299147217f7cb9f48410c4877a8956db6ce25
Min Thuta Naing <122962303+minthutanaing@users.noreply.github.com>
2023-05-02 03:09:00 +0000

    rework

---
+++ src/MainApp.java
@@ -0,0 +1,6 @@
+public class MainApp {
+    public static void main (String[] args){
+        Hotel hotel = new Hotel(); 
+        hotel.start();
+    }
+}
--- src/Room.java
+++ src/Room.java
@@ -1,18 +1,24 @@
-import java.util.*;
-
 public class Room {
-    protected String roomType; 
-    protected int numRoom; 
-    protected Set<Integer> roomNumSet; 
+    private String roomType;
+    private int roomNum;
+    private boolean availability;
 
-    public Room (String roomType, int numRoom, Set<Integer> roomNumSet){
-        this.roomType = roomType; 
-        this.numRoom = numRoom; 
-        this.roomNumSet = roomNumSet;
-
+    public Room (String roomType, int roomNum){
+        this.roomType = roomType;
+        this.roomNum = roomNum;
+        this.availability = true;
     }
 
-    public String getRoomType() {return roomType;}
-    public int getNumRoom() {return numRoom;}
-    public Set<Integer> getRoomNumSet() {return roomNumSet;}
+    public String getRoomType() {return roomType; }
+    public void setRoomType(String roomType) {this.roomType = roomType; }
+    public int getRoomNum() {return roomNum;}
+    public void setRoomNum(int roomNum) {this.roomNum = roomNum; }
+    public boolean isRoomAvailable() { 
+        return availability;
+    }
+
+    public void setAvailability(boolean availability){
+        this.availability = availability;
+    }
+
 }
